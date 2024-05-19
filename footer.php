ff851c39adc537f65bdd8886ec359d2baec9b5ce
Dizon <alfredsondizon889@gmail.com>
2024-05-19 19:47:05 +0800

    Test Commit

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
     </div>
 </div>
 
-<!-- Update Account Modal -->
+<!-- Update Account Modals -->
 <div class="modal fade" id="update_account_modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content bg-dark text-white">
